f6622f99dd34e19ee1b3d87e46459309e3dd14e2
magasaniganeshkumar <83356306+magasaniganeshkumar@users.noreply.github.com>
2021-07-31 16:54:30 +0530

    solved and added case statement

--- employee_wages.py
+++ employee_wages.py
@@ -46,3 +46,67 @@ else:
     logging.info(total_wage)
     logging.info("new request process completed...!")
     print("Part time Daily Employee wage  is : ", total_wage)
+
+
+# solving use case statement
+print("\tWelcome to Employee Wage Computation ")
+
+while True:
+    try:
+        print("you please choose below options to process : ")
+        print("""1.daily employee Wage
+                 2.daily wages for PartTime employee
+                 3.exit
+              """)
+        option = int(input("you please enter your options to process : "))
+        list_number = [1, 2, 3]
+
+        if option not in list_number:
+            print("you please enter valid input ")
+            break
+    except Exception as massage:
+        print("you please enter valid input ")
+        logging.exception(massage)
+    else:
+        if option == 1:
+            random_number = random.randint(0, 1)
+            if random_number == 1:
+                try:
+                    day_hours = int(input(" Please enter working hours for one day ! : "))
+                    wage_per_hour = int(input(" Please enter wage for hour ! : "))
+                except Exception as massage:
+                    print("Please enter valid input , you need to enter number only......!")
+                    logging.info(massage)
+                else:
+                    total_wage = day_hours * wage_per_hour
+                    logging.info(total_wage)
+                    logging.info("new request process completed...!")
+                    print("Daily Employee wage  is : ", total_wage)
+            else:
+                print("Employee is Absent.....!")
+                logging.info("Employee is Absent.....!")
+
+        if option == 2:
+            random_number = random.randint(0, 1)
+            if random_number == 1:
+                try:
+                    part_time_day_hours = int(input(
+                        " Please enter working 'PartTime' hours for  one day ! : "))
+                    wage_per_hour = int(input(" Please enter wage for hour ! : "))
+                except Exception as massage:
+                    print("Please enter valid input , you need to enter number only......!")
+                    logging.info(massage)
+                else:
+                    total_wage = part_time_day_hours * wage_per_hour
+                    logging.info(total_wage)
+                    logging.info("new request process completed...!")
+                    print("Part time Daily Employee wage  is : ", total_wage)
+            else:
+                print("Employee is Absent.....!")
+                logging.info("Employee is Absent.....!")
+
+        else:
+            logging.info(
+                "New Employee wage  request processing completed.....!")
+            break
+print("Thanks for using our application .....!")
